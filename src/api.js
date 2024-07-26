8ffaa68a0d0ea2cdbe564d6c805584fb4c9224e8
Derek Papierski <65932522+d-pap@users.noreply.github.com>
2024-07-26 11:38:25 -0400

    add click functionality to problems and add problem solving page when a problem is clicked

--- src/api.js
+++ src/api.js
@@ -12,19 +12,6 @@ const fetchProblems = async () => {
     return response.data
   } catch (error) {
     console.error("Error fetching problems:", error)
-    if (error.response) {
-      // The request was made and the server responded with a status code
-      // that falls out of the range of 2xx
-      console.error("Error response data:", error.response.data)
-      console.error("Error response status:", error.response.status)
-      console.error("Error response headers:", error.response.headers)
-    } else if (error.request) {
-      // The request was made but no response was received
-      console.error("Error request data:", error.request)
-    } else {
-      // Something happened in setting up the request that triggered an Error
-      console.error("Unexpected error:", error.message)
-    }
     throw error
   }
 }
