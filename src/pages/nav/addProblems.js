1ed064b21b25d60b5270b555a1885cf5dbd65c6a
Marvin <mlakham@umich.edu>
2024-07-30 16:15:56 -0400

    Resolved merge conflicts

--- src/pages/nav/addProblems.js
+++ src/pages/nav/addProblems.js
@@ -79,138 +79,134 @@ const AddProblems = () => {
     window.location.reload()
   }
 
-    return (
-        <Box component="section" display={"flex"} padding={3} width={"95%"}>
-            <form onSubmit={handleSubmit} style={{width: "100%"}}>
-                <Stack spacing={2}>
-                    <label>Title: </label>
-                    <TextField
-                        type="text"
-                        name="title"
-                        value={formData.title}
-                        onChange={handleChange}
-                        required
-                        label="Please input a title"
-                        variant="outlined"
-                    />
-                    <label>Problem Description: </label>
-                    <TextField
-                        name="problemDescription"
-                        value={formData.problemDescription}
-                        onChange={handleChange}
-                        required
-                        label="Please input a problem description"
-                        variant="outlined"
-                    />
-                    <label>Time Limit: </label>
-                    <TextField
-                        type="text"
-                        name="timeLimit"
-                        value={formData.timeLimit}
-                        onChange={handleChange}
-                        label="Please input a time limit"
-                        variant="outlined"
-                    />
-                    <label>Memory Limit: </label>
-                    <TextField
-                        type="text"
-                        name="memoryLimit"
-                        value={formData.memoryLimit}
-                        onChange={handleChange}
-                        label="Please input a memory limit"
-                        variant="outlined"
-                    />
-                    <label>Input Description: </label>
-                    <TextField
-                        name="inputDescription"
-                        value={formData.inputDescription}
-                        onChange={handleChange}
-                        label="Please input a input description"
-                        variant="outlined"
-                    />
-                    <label>Output Description: </label>
-                    <TextField
-                        name="outputDescription"
-                        value={formData.outputDescription}
-                        onChange={handleChange}
-                        label="Please input a output description"
-                        variant="outlined"
-                    />
-                    <label>Video: </label>
-                    <TextField
-                        type="text"
-                        name="videoLink"
-                        value={formData.videoLink}
-                        onChange={handleChange}
-                        label="Please input a video link"
-                        variant="outlined"
-                    />
-                    <div>
-                        <label>Test Cases: </label>
-                        <Button
-                            variant="contained"
-                            type="button"
-                            onClick={addTestCase}
-                        >
-                            Add Test Case
-                        </Button>
-                        <Stack
-                            direction="column"
-                            justifyContent="center"
-                            alignItems="center"
-                            spacing={3}
-                        >
-                            {formData.testCases.map((testCase, idx) => (
-                                <Stack
-                                    key={idx}
-                                    direction="row"
-                                    justifyContent="center"
-                                    alignItems="center"
-                                    spacing={2}
-                                >
-                                    <label>Input: </label>
-                                    <TextField
-                                        type="text"
-                                        name="input"
-                                        value={testCase.input}
-                                        onChange={(e) =>
-                                            handleTestCaseChange(idx, e)
-                                        }
-                                        label=""
-                                        variant="standard"
-                                    />
-                                    <label>Output: </label>
-                                    <TextField
-                                        type="text"
-                                        name="output"
-                                        value={testCase.output}
-                                        onChange={(e) =>
-                                            handleTestCaseChange(idx, e)
-                                        }
-                                        label=""
-                                        variant="standard"
-                                    />
-                                    <Button
-                                        variant="contained"
-                                        type="button"
-                                        onClick={() => removeTestCase(idx)}
-                                    >
-                                        Remove
-                                    </Button>
-                                </Stack>
-                            ))}
-                        </Stack>
-                    </div>
+  return (
+    <Box component="section" display={"flex"} padding={3} width={"95%"}>
+      <form onSubmit={handleSubmit} style={{ width: "100%" }}>
+        <Stack spacing={2}>
+          <label>Title: </label>
+          <TextField
+            type="text"
+            name="title"
+            value={formData.title}
+            onChange={handleChange}
+            required
+            label="Please input a title"
+            variant="outlined"
+          />
+          <label>Problem Description: </label>
+          <TextField
+            name="problemDescription"
+            value={formData.problemDescription}
+            onChange={handleChange}
+            required
+            label="Please input a problem description"
+            variant="outlined"
+          />
+          <label>Time Limit: </label>
+          <TextField
+            type="text"
+            name="timeLimit"
+            value={formData.timeLimit}
+            onChange={handleChange}
+            label="Please input a time limit"
+            variant="outlined"
+          />
+          <label>Memory Limit: </label>
+          <TextField
+            type="text"
+            name="memoryLimit"
+            value={formData.memoryLimit}
+            onChange={handleChange}
+            label="Please input a memory limit"
+            variant="outlined"
+          />
+          <label>Input Description: </label>
+          <TextField
+            name="inputDescription"
+            value={formData.inputDescription}
+            onChange={handleChange}
+            label="Please input a input description"
+            variant="outlined"
+          />
+          <label>Output Description: </label>
+          <TextField
+            name="outputDescription"
+            value={formData.outputDescription}
+            onChange={handleChange}
+            label="Please input a output description"
+            variant="outlined"
+          />
+          <label>Video: </label>
+          <TextField
+            type="text"
+            name="videoLink"
+            value={formData.videoLink}
+            onChange={handleChange}
+            label="Please input a video link"
+            variant="outlined"
+          />
+          <div>
+            <label>Test Cases: </label>
+            <Button
+              variant="contained"
+              type="button"
+              onClick={addTestCase}
+            >
+              Add Test Case
+            </Button>
+            <Stack
+              direction="column"
+              justifyContent="center"
+              alignItems="center"
+              spacing={3}
+            >
+              {formData.testCases.map((testCase, idx) => (
+                <Stack
+                  key={idx}
+                  direction="row"
+                  justifyContent="center"
+                  alignItems="center"
+                  spacing={2}
+                >
+                  <label>Input: </label>
+                  <TextField
+                    type="text"
+                    name="input"
+                    value={testCase.input}
+                    onChange={(e) =>
+                      handleTestCaseChange(idx, e)
+                    }
+                    label=""
+                    variant="standard"
+                  />
+                  <label>Output: </label>
+                  <TextField
+                    type="text"
+                    name="output"
+                    value={testCase.output}
+                    onChange={(e) =>
+                      handleTestCaseChange(idx, e)
+                    }
+                    label=""
+                    variant="standard"
+                  />
+                  <Button
+                    variant="contained"
+                    type="button"
+                    onClick={() => removeTestCase(idx)}
+                  >
+                    Remove
+                  </Button>
                 </Stack>
               ))}
             </Stack>
           </div>
+          <Button variant="contained" type="submit">
+            Submit
+          </Button>
         </Stack>
-        <Button variant="contained" type="submit">
-          Submit
-        </Button>
       </form>
-    </Box>
+    </Box >
   )
 }
 
