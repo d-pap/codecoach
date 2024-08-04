89c8fabc66ebc5d8b3c7166824ac1e3e116a109b
Marfn Mlakha <mlakham@umich.edu>
2024-08-04 18:03:58 -0400

    Refactored to match base

--- src/components/problems/problem-types/ICPC.js
+++ src/components/problems/problem-types/ICPC.js
@@ -8,7 +8,7 @@ import { useLocation } from 'react-router-dom'
 import ProblemCard from '../ProblemCardLayout'
 import { ICPCFilter, ICPCFilterDisplay } from './problem-filters/ICPCFilter'
 import { Stack } from '@mui/material'
-import ResizableColumn from '../../formating/ResizableColumn'
+import ResizableColumn from '../../utility/ResizableColumn'
 
 function ICPC() {
   const location = useLocation()
@@ -20,6 +20,34 @@ function ICPC() {
   useEffect(() => {
     setFilteredProblems(ICPCFilter(problems, region, year))
   }, [problems, region, year])
+  const [loading, setLoading] = useState(true)
+  const [error, setError] = useState(null)
+  const [resizableColumnProps, setResizableColumnProps] = useState({
+    initialWidth: 0,
+    minWidth: 0,
+    maxWidth: 0,
+  })
+
+  useEffect(() => {
+    function getResizableColumnProps() {
+      const windowWidth = window.innerWidth
+
+      // Calculate dimensions based on window width
+      const minWidth = windowWidth * 0.2
+      const maxWidth = windowWidth * 0.3
+      const initialWidth = windowWidth * 0.25
+
+      return { initialWidth, maxWidth, minWidth }
+    }
+
+    const updateColumnProps = () => {
+      setResizableColumnProps(getResizableColumnProps())
+    }
+
+    updateColumnProps()
+    window.addEventListener('resize', updateColumnProps)
+    return () => window.removeEventListener('resize', updateColumnProps)
+  }, [])
 
   const handleRegionChange = (event) => {
     setRegion(event.target.value)
@@ -35,7 +63,11 @@ function ICPC() {
         <h1>ICPC Problems</h1>
       </div>
       <div className="container" style={{ display: 'flex' }}>
-        <ResizableColumn initialWidth={200}>
+        <ResizableColumn
+          initialWidth={resizableColumnProps.initialWidth}
+          minWidth={resizableColumnProps.minWidth}
+          maxWidth={resizableColumnProps.maxWidth}
+        >
           <h3>Filters</h3>
           <ICPCFilterDisplay
             region={region}
