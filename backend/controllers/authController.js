31b8d1451c3d382bf42d8068fcb16ae192f654ad
Marfn Mlakha <mlakham@umich.edu>
2024-08-05 13:17:38 -0400

    Deleted old repo and cloned the other
    New history starts here

--- backend/controllers/authController.js
+++
@@ -1,5 +0,0 @@
-/* Controller for authorization tasks
-- register and create a new user
-- log in to an existing account
-- log out of account
-*/
--- backend/routes/submissionRoutes.js
+++
@@ -1,4 +0,0 @@
-/* Routes for submission-based tasks
-- /api/submissions - run code submission and get results
-- /api/submissions/:id - see result of a specific code submission
-*/
