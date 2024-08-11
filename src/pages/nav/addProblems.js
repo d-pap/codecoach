a3e1febb400cbdf477f245269b4fd63b29822cb6
Marfn Mlakha <mlakham@umich.edu>
2024-08-11 14:37:10 -0400

    Bug fixes
    Fixed the issue causing extra white space to the right of the website. Made the problems page cards wider. Finalized icpc problem json layout. Added selectable regions and years. Removed chatbox. Added vertical scroll bar.

--- src/pages/nav/addProblems.js
+++ src/pages/nav/addProblems.js
@@ -1,5 +1,14 @@
 import React, { useState } from 'react'
-import { Box, Stack, Button, TextField } from '@mui/material'
+import {
+  Box,
+  Stack,
+  Button,
+  TextField,
+  InputLabel,
+  MenuItem,
+  Select,
+  FormControl,
+} from '@mui/material'
 import TestCase from '../../components/add-problems/TestCase'
 import {
   handleChange,
@@ -11,6 +20,7 @@ import {
 
 const AddProblems = () => {
   const [formData, setFormData] = useState({
+    type: 'icpc', // defult type is icpc
     title: '',
     timeLimit: '',
     memoryLimit: '',
@@ -19,7 +29,61 @@ const AddProblems = () => {
     outputDescription: '',
     videoLink: '',
     testCases: [{ input: '', output: '' }],
+    comments: '',
+    contestRegion: '',
+    contestSubRegion: '',
+    contestYear: '',
+    hint: '',
   })
+
+  const subregions = {
+    'World Finals': ['ICPC World Finals'],
+    'Europe Contests': [
+      'European Championship',
+      'Central Europe Regional Contest (CERC)',
+      'Northern Eurasia Finals (NERC)',
+      'Northwestern Europe Regional Contest (NWERC)',
+      'Southeastern Europe Regional Contest (SEERC)',
+      'Southwestern Europe Regional Contest (SWERC)',
+      'Benelux Algorithm Programming Contest (BAPC)',
+      'CTU Open Contest (Czech Technical University)',
+      'German Collegiate Programming Contest (GCPC)',
+      'Nordic Collegiate Programming Contest (NCPC)',
+      'UK and Ireland Programming Contest (UKIPC)',
+    ],
+    'Asia Pacific Contests': [
+      'Asia Pacific Championship',
+      'Indonesia',
+      'Japan',
+      'Philippines',
+      'Singapore',
+      'South Korea',
+      'Taiwan',
+      'Vietnam',
+    ],
+    'Asia East Continent Contests': [
+      'Hangzhou',
+      'Hefei',
+      'Hongkong',
+      'Jinan',
+      'Macau',
+      'Nanjing',
+      'Shanghai',
+      'Shenyang',
+      'Yinchuan',
+    ],
+    'North America Contests': [
+      'North America Championship',
+      'Mid-Atlantic USA Regional Contest',
+      'North Central Regional Contest',
+      'Rocky Mountain Regional Contest',
+    ],
+    'Latin American Contests': [
+      'Latin America Championship',
+      'Latin American Regional Contest',
+    ],
+    'Africa and Arab Contests': ['Arab Collegiate Programming Championship'],
+  }
 
   return (
     <Box component="section" display={'flex'} padding={3} width={'95%'}>
@@ -47,7 +111,7 @@ const AddProblems = () => {
             label="Please input a problem description"
             variant="outlined"
           />
-          <label>Time Limit: </label>
+          <label>Time and Memory Limits: </label>
           <TextField
             type="text"
             name="timeLimit"
@@ -56,7 +120,6 @@ const AddProblems = () => {
             label="Please input a time limit"
             variant="outlined"
           />
-          <label>Memory Limit: </label>
           <TextField
             type="text"
             name="memoryLimit"
@@ -65,7 +128,7 @@ const AddProblems = () => {
             label="Please input a memory limit"
             variant="outlined"
           />
-          <label>Input Description: </label>
+          <label>Input and Output Description: </label>
           <TextField
             name="inputDescription"
             value={formData.inputDescription}
@@ -73,7 +136,6 @@ const AddProblems = () => {
             label="Please input an input description"
             variant="outlined"
           />
-          <label>Output Description: </label>
           <TextField
             name="outputDescription"
             value={formData.outputDescription}
@@ -81,15 +143,7 @@ const AddProblems = () => {
             label="Please input an output description"
             variant="outlined"
           />
-          <label>Video: </label>
-          <TextField
-            type="text"
-            name="videoLink"
-            value={formData.videoLink}
-            onChange={(e) => handleChange(e, formData, setFormData)}
-            label="Please input a video link"
-            variant="outlined"
-          />
+
           <div>
             <label>Test Cases: </label>
             <Button
@@ -120,6 +174,96 @@ const AddProblems = () => {
               ))}
             </Stack>
           </div>
+          <label>Additional comments: </label>
+          <TextField
+            name="comments"
+            value={formData.comments}
+            onChange={(e) => handleChange(e, formData, setFormData)}
+            label="Please input any additional comments"
+            variant="outlined"
+          />
+
+          <label>Region and Year: </label>
+          <FormControl fullWidth>
+            <InputLabel id="region-label">Contest Region</InputLabel>
+            <Select
+              required
+              labelId="region-label"
+              name="contestRegion"
+              label="Contest Region"
+              value={formData.contestRegion}
+              onChange={(e) => handleChange(e, formData, setFormData)}
+            >
+              <MenuItem value="World Finals">World Finals</MenuItem>
+              <MenuItem value="Europe Contests">Europe Contests</MenuItem>
+              <MenuItem value="Asia Pacific Contests">
+                Asia Pacific Contests
+              </MenuItem>
+              <MenuItem value="Asia East Continent Contests">
+                Asia East Continent Contests
+              </MenuItem>
+              <MenuItem value="North America Contests">
+                North America Contests
+              </MenuItem>
+              <MenuItem value="Latin American Contests">
+                Latin American Contests
+              </MenuItem>
+              <MenuItem value="Africa and Arab Contests">
+                Africa and Arab Contests
+              </MenuItem>
+            </Select>
+          </FormControl>
+
+          <FormControl fullWidth>
+            <InputLabel id="sub-region-label">Contest Sub Region</InputLabel>
+            <Select
+              required
+              labelId="sub-region-label"
+              name="contestSubRegion"
+              label="Contest Sub Region"
+              value={formData.contestSubRegion}
+              onChange={(e) => handleChange(e, formData, setFormData)}
+              disabled={!formData.contestRegion} // Disable if no region is selected
+            >
+              {subregions[formData.contestRegion]?.map((subregion) => (
+                <MenuItem key={subregion} value={subregion}>
+                  {subregion}
+                </MenuItem>
+              ))}
+            </Select>
+          </FormControl>
+
+          <TextField
+            required
+            type="number" // Change type to number
+            name="contestYear"
+            value={formData.contestYear}
+            onChange={(e) => handleChange(e, formData, setFormData)}
+            label="Please input a contest year"
+            variant="outlined"
+            inputProps={{
+              min: 2000, // Set minimum year
+              max: 2030, // Set maximum year
+            }}
+          />
+
+          <label>Hint/Solution: </label>
+          <TextField
+            name="hint"
+            value={formData.hint}
+            onChange={(e) => handleChange(e, formData, setFormData)}
+            label="Please input a hint/solution"
+            variant="outlined"
+          />
+          <TextField
+            type="text"
+            name="videoLink"
+            value={formData.videoLink}
+            onChange={(e) => handleChange(e, formData, setFormData)}
+            label="Please input a video link"
+            variant="outlined"
+          />
+
           <Button variant="contained" type="submit">
             Submit
           </Button>
