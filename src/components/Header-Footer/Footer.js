6f4c6910cab865f3e2e8fcb81441eca7bdf32f80
Derek Papierski <65932522+d-pap@users.noreply.github.com>
2024-08-15 12:28:18 -0400

    update font style of footer text

--- src/components/Header-Footer/Footer.js
+++ src/components/Header-Footer/Footer.js
@@ -7,19 +7,20 @@ import InstagramIcon from '@mui/icons-material/Instagram'
 
 const FooterContainer = styled(Box)(({ theme }) => ({
   backgroundColor: '#468585',
-  color: theme.palette.primary.contrastText,
-  padding: theme.spacing(4, 0),
+  color: theme.palette.primary.contrastText, // right now, this is using MUI's default theme styles. so this would be white
+  padding: theme.spacing(4, 0), // padding top and bottom=32px (4*8px=32), no padding on the sides
   marginTop: 'auto',
 }))
 
 const FooterTypography = styled(Typography)({
   marginBottom: '0.5rem',
   color: '#def9c4',
-  fontFamily: 'Inter, sans-serif',
+  fontFamily: 'Inter, sans-serif', // Inter for headings
 })
 
 const EmailLink = styled(Link)({
   color: '#9cdba6',
+  fontFamily: 'DM Sans, sans-serif', // DM Sans for link font (same as body text)
   textDecoration: 'none',
   '&:hover': {
     color: '#878686',
@@ -43,18 +44,24 @@ const Footer = () => {
   return (
     <FooterContainer>
       <Container maxWidth="lg">
-        <Grid container spacing={4} justifyContent="center">
+        <Grid container spacing={12} justifyContent="center">
           <Grid item xs={12} sm={6} md={4}>
             <FooterTypography variant="h6" gutterBottom align="left">
               Contact Us
             </FooterTypography>
-            <FooterTypography align="left">
+            <FooterTypography
+              align="left"
+              sx={{ fontFamily: 'DM Sans, sans-serif' }} // DM Sans for body text
+            >
               General Inquiries:{' '}
               <EmailLink href="mailto:team@codecoach.com">
                 team@codecoach.com
               </EmailLink>
             </FooterTypography>
-            <FooterTypography align="left">
+            <FooterTypography
+              align="left"
+              sx={{ fontFamily: 'DM Sans, sans-serif' }} // DM Sans for body text
+            >
               Support:{' '}
               <EmailLink href="mailto:support@codecoach.com">
                 support@codecoach.com
