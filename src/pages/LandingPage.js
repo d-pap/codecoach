1cd8ff721647cb02a1e9d303002478456a1bbab8
Derek Papierski <65932522+d-pap@users.noreply.github.com>
2024-08-29 12:09:37 -0400

    changed landing page hero section

--- src/pages/LandingPage.js
+++ src/pages/LandingPage.js
@@ -14,7 +14,8 @@ import {
   Typography,
 } from '@mui/material'
 import { styled } from '@mui/system'
-import HeroBg from '../images/hero-bg.jpg'
+import Trophy from '../images/icpc-trophy.png'
+import KeyboardDoubleArrowRightIcon from '@mui/icons-material/KeyboardDoubleArrowRight'
 
 const HeaderSection = styled(AppBar)(({ theme }) => ({
   background: 'transparent',
@@ -22,18 +23,6 @@ const HeaderSection = styled(AppBar)(({ theme }) => ({
   justifyContent: 'space-between',
   boxShadow: 'none',
   color: theme.palette.text.primary,
-}))
-
-const HeroSection = styled(Box)(({ theme }) => ({
-  backgroundImage: `url(${HeroBg})`,
-  backgroundSize: 'cover',
-  backgroundPosition: 'center',
-  height: '60vh',
-  display: 'flex',
-  alignItems: 'center',
-  justifyContent: 'center',
-  color: theme.palette.text.primary,
-  textAlign: 'center',
 }))
 
 const FeatureCard = styled(Box)(({ theme }) => ({
@@ -47,6 +36,97 @@ const FeatureCard = styled(Box)(({ theme }) => ({
   borderRadius: theme.spacing(2),
   boxShadow: theme.shadows[3],
 }))
+
+const HighlightedText = styled('span')(({ theme }) => ({
+  backgroundColor: theme.palette.text.primary,
+  color: theme.palette.common.white,
+  padding: theme.spacing(0.5, 2), // padding around the text
+  borderRadius: theme.spacing(5),
+}))
+
+const ImageContainer = styled(Box)(({ theme }) => ({
+  position: 'relative', // positioning for the image inside
+  width: '50%',
+  height: '350px',
+  padding: theme.spacing(2),
+  background: 'radial-gradient(circle, #fffffe, #6C63FF)', //6C63FF
+  borderRadius: theme.spacing(2),
+  boxShadow: '0 4px 8px rgba(0, 0, 0, 0.1)', // optional shadow for 3D effect
+  overflow: 'unset', // allows the image to overflow the container
+}))
+
+function HeroSection({ handleOpenRegister }) {
+  return (
+    <Container
+      maxWidth="lg"
+      sx={{
+        display: 'flex',
+        flexDirection: { xs: 'column', md: 'row' }, // stack on mobile
+        alignItems: 'center',
+        justifyContent: 'space-between',
+        height: 'auto',
+        padding: (theme) => theme.spacing(4),
+        py: 8,
+      }}
+    >
+      {/* left side with text */}
+      <Box
+        sx={{
+          width: '40%',
+          textAlign: { xs: 'center', md: 'left' },
+          mb: { xs: 3, md: 0 },
+        }}
+      >
+        <Typography
+          variant="h3"
+          component="h1"
+          gutterBottom
+          sx={{
+            lineHeight: 1.2,
+          }}
+        >
+          Where Coders become <HighlightedText>Champions</HighlightedText>
+        </Typography>
+        <Typography
+          variant="h6"
+          component="h2"
+          gutterBottom
+          sx={{
+            color: 'text.secondary',
+            mb: 3,
+          }}
+        >
+          Experience the next level of competitive programming preparation with
+          CodeCoach.
+        </Typography>
+        <Button
+          variant="contained"
+          size="large"
+          onClick={handleOpenRegister}
+          sx={{}}
+        >
+          Get started for free <KeyboardDoubleArrowRightIcon sx={{ ml: 1 }} />
+        </Button>
+      </Box>
+
+      {/* right side with image */}
+      <ImageContainer>
+        <Box
+          component="img"
+          src={Trophy}
+          alt="AI Robot"
+          sx={{
+            position: 'absolute',
+            bottom: 0,
+            zIndex: 1,
+            width: { xs: '400px', md: '700px', lg: '800px' },
+            height: 'auto',
+          }}
+        />
+      </ImageContainer>
+    </Container>
+  )
+}
 
 const LandingPage = ({ onLogin, onRegister }) => {
   const [openLogin, setOpenLogin] = useState(false)
@@ -81,135 +161,121 @@ const LandingPage = ({ onLogin, onRegister }) => {
           </Button>
         </Toolbar>
       </HeaderSection>
+      <Box sx={{ bgcolor: 'background.default', py: 6 }}>
+        {/* hero section */}
+        <HeroSection handleOpenRegister={handleOpenRegister} />
 
-      <HeroSection>
-        <Container>
-          <Typography variant="h2" component="h1" gutterBottom>
-            Master Your Coding Skills
-          </Typography>
-          <Typography variant="h5" component="h2" gutterBottom>
-            Prepare, Learn, and Excel with CodeCoach
-          </Typography>
-          <Button
-            variant="contained"
-            size="large"
-            sx={{ mt: 2 }}
-            onClick={handleOpenRegister}
-          >
-            Get Started
-          </Button>
+        {/* feature section */}
+        <Container sx={{ mt: 8, mb: 8 }}>
+          <Grid container spacing={4}>
+            <Grid item xs={12} md={4}>
+              <FeatureCard>
+                <Typography variant="h5" component="h3" gutterBottom>
+                  Diverse Problem Set
+                </Typography>
+                <Typography>
+                  Access a wide range of coding challenges across various
+                  difficulty levels and topics.
+                </Typography>
+              </FeatureCard>
+            </Grid>
+            <Grid item xs={12} md={4}>
+              <FeatureCard>
+                <Typography variant="h5" component="h3" gutterBottom>
+                  Real-time Compiler
+                </Typography>
+                <Typography>
+                  Write, run, and test your code directly in the browser with
+                  our powerful compiler.
+                </Typography>
+              </FeatureCard>
+            </Grid>
+            <Grid item xs={12} md={4}>
+              <FeatureCard>
+                <Typography variant="h5" component="h3" gutterBottom>
+                  Track Your Progress
+                </Typography>
+                <Typography>
+                  Monitor your improvement and compare your solutions with
+                  others in the community.
+                </Typography>
+              </FeatureCard>
+            </Grid>
+          </Grid>
         </Container>
-      </HeroSection>
 
-      <Container sx={{ mt: 8, mb: 8 }}>
-        <Grid container spacing={4}>
-          <Grid item xs={12} md={4}>
-            <FeatureCard>
-              <Typography variant="h5" component="h3" gutterBottom>
-                Diverse Problem Set
-              </Typography>
-              <Typography>
-                Access a wide range of coding challenges across various
-                difficulty levels and topics.
-              </Typography>
-            </FeatureCard>
-          </Grid>
-          <Grid item xs={12} md={4}>
-            <FeatureCard>
-              <Typography variant="h5" component="h3" gutterBottom>
-                Real-time Compiler
-              </Typography>
-              <Typography>
-                Write, run, and test your code directly in the browser with our
-                powerful compiler.
-              </Typography>
-            </FeatureCard>
-          </Grid>
-          <Grid item xs={12} md={4}>
-            <FeatureCard>
-              <Typography variant="h5" component="h3" gutterBottom>
-                Track Your Progress
-              </Typography>
-              <Typography>
-                Monitor your improvement and compare your solutions with others
-                in the community.
-              </Typography>
-            </FeatureCard>
-          </Grid>
-        </Grid>
-      </Container>
+        <Dialog open={openLogin} onClose={handleCloseLogin}>
+          <DialogTitle>Login</DialogTitle>
+          <DialogContent>
+            <TextField
+              autoFocus
+              margin="dense"
+              id="email"
+              label="Email Address"
+              type="email"
+              fullWidth
+              variant="outlined"
+            />
+            <TextField
+              margin="dense"
+              id="password"
+              label="Password"
+              type="password"
+              fullWidth
+              variant="outlined"
+            />
+          </DialogContent>
+          <DialogActions>
+            <Button onClick={handleCloseLogin}>Cancel</Button>
+            <Button onClick={handleLogin}>Login</Button>
+          </DialogActions>
+        </Dialog>
 
-      <Dialog open={openLogin} onClose={handleCloseLogin}>
-        <DialogTitle>Login</DialogTitle>
-        <DialogContent>
-          <TextField
-            autoFocus
-            margin="dense"
-            id="email"
-            label="Email Address"
-            type="email"
-            fullWidth
-            variant="outlined"
-          />
-          <TextField
-            margin="dense"
-            id="password"
-            label="Password"
-            type="password"
-            fullWidth
-            variant="outlined"
-          />
-        </DialogContent>
-        <DialogActions>
-          <Button onClick={handleCloseLogin}>Cancel</Button>
-          <Button onClick={handleLogin}>Login</Button>
-        </DialogActions>
-      </Dialog>
-
-      <Dialog open={openRegister} onClose={handleCloseRegister}>
-        <DialogTitle>Account Details</DialogTitle>
-        <DialogContent>
-          <TextField
-            autoFocus
-            margin="dense"
-            id="name"
-            label="Full Name"
-            type="text"
-            fullWidth
-            variant="outlined"
-          />
-          <TextField
-            margin="dense"
-            id="email"
-            label="Email Address"
-            type="email"
-            fullWidth
-            variant="outlined"
-          />
-          <TextField
-            margin="dense"
-            id="password"
-            label="Password"
-            type="password"
-            fullWidth
-            variant="outlined"
-          />
-          <TextField
-            margin="dense"
-            id="confirmPassword"
-            label="Confirm Password"
-            type="password"
-            fullWidth
-            variant="outlined"
-          />
-        </DialogContent>
-        <DialogActions>
-          <Button onClick={handleCloseRegister}>Cancel</Button>
-          <Button variant="contained" onClick={handleRegister}>
-            Register
-          </Button>
-        </DialogActions>
-      </Dialog>
+        <Dialog open={openRegister} onClose={handleCloseRegister}>
+          <DialogTitle>Account Details</DialogTitle>
+          <DialogContent>
+            <TextField
+              autoFocus
+              margin="dense"
+              id="name"
+              label="Full Name"
+              type="text"
+              fullWidth
+              variant="outlined"
+            />
+            <TextField
+              margin="dense"
+              id="email"
+              label="Email Address"
+              type="email"
+              fullWidth
+              variant="outlined"
+            />
+            <TextField
+              margin="dense"
+              id="password"
+              label="Password"
+              type="password"
+              fullWidth
+              variant="outlined"
+            />
+            <TextField
+              margin="dense"
+              id="confirmPassword"
+              label="Confirm Password"
+              type="password"
+              fullWidth
+              variant="outlined"
+            />
+          </DialogContent>
+          <DialogActions>
+            <Button onClick={handleCloseRegister}>Cancel</Button>
+            <Button variant="contained" onClick={handleRegister}>
+              Register
+            </Button>
+          </DialogActions>
+        </Dialog>
+      </Box>
     </Box>
   )
 }
