a33cef2c00d3df72a1ca05a16404eb4f825e9d2e
Derek Papierski <65932522+d-pap@users.noreply.github.com>
2024-09-17 10:44:13 -0400

    ddd

--- src/pages/problems/problem-types/Interview.js
+++ src/pages/problems/problem-types/Interview.js
@@ -3,14 +3,25 @@
  * The user can filter the questions by ... (to be determined based on data format)
  */
 
+import { Box, Container, Typography } from '@mui/material'
 import React from 'react'
 //import { InterviewFilter } from '../../../components/problems/problem-filters/InterviewFilter'
 
 function Interview() {
   return (
-    <div>
-      <h1>Interview Questions</h1>
-    </div>
+    <Box sx={{ bgcolor: 'background.default', py: 6 }}>
+      <Container maxWidth="lg">
+        <Typography
+          variant="h2"
+          component="h1"
+          gutterBottom
+          align="center"
+          sx={{ mb: 6 }}
+        >
+          Interview Questions
+        </Typography>
+      </Container>
+    </Box>
   )
 }
 
