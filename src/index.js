e757c07299d2106489edc0357ca59da1a363ec57
Derek Papierski <65932522+d-pap@users.noreply.github.com>
2024-09-26 13:42:27 -0400

    acmDemo before merging lazy loading into it

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 /**
  * Main entry point of the react app that renders App.js
  */
-
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 //import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
