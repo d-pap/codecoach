51168545c18dd50aed4179a38971a6e08fe84a78
Marvin <mlakham@umich.edu>
2024-11-19 22:20:40 -0500

    Copy Resume
    A button will allow users to copy the resume. The resume is stripped of it's markdown before being copied.

--- src/pages/Resume.js
+++ src/pages/Resume.js
@@ -1,48 +1,24 @@
-import React, { useState, useRef, useEffect, Suspense, lazy } from 'react'
+// Resume.js
+
+import React, { useState, useRef, useEffect, Suspense } from 'react'
 import { sendResume } from '../api'
 import {
   Container,
   Typography,
-  Button,
-  Paper,
-  LinearProgress,
 } from '@mui/material'
-import DoubleArrowRoundedIcon from '@mui/icons-material/DoubleArrowRounded'
-import { useTheme } from '@mui/material/styles'
-import ReactMarkdown from 'react-markdown'
+import removeMarkdown from 'remove-markdown' // Import the remove-markdown library
 import DOMPurify from 'dompurify'
+
+import ResumeForm from '../components/resume/ResumeForm'
+import ResumeResult from '../components/resume/ResumeResult'
 import CenteredCircleLoader from '../components/utility/CenteredLoader'
 
-// Lazy-loaded components
-const Description = lazy(() => import('../components/resume/Description'))
-const WorkExperience = lazy(() => import('../components/resume/WorkExperience'))
-const Education = lazy(() => import('../components/resume/Education'))
-const Skills = lazy(() => import('../components/resume/Skills'))
-const Projects = lazy(() => import('../components/resume/Projects'))
-const AdditionalActivities = lazy(
-  () => import('../components/resume/AdditionalActivities')
-)
-const AdditionalComments = lazy(
-  () => import('../components/resume/AdditionalComments')
-)
-const JobDescription = lazy(() => import('../components/resume/JobDescription'))
-const SensitiveInfoWarning = lazy(
+const SensitiveInfoWarning = React.lazy(
   () => import('../components/resume/SensitiveInfoWarning')
 )
 
-const SafeMarkdown = ({ content }) => {
-  const sanitizedContent = DOMPurify.sanitize(content)
-  return (
-    <ReactMarkdown
-      children={sanitizedContent}
-      components={{
-        hr: () => null,
-      }}
-    />
-  )
-}
-
 const Resume = () => {
+  // State hooks for resume fields
   const [description, setDescription] = useState('')
   const [workExperiences, setWorkExperiences] = useState([])
   const [educations, setEducations] = useState([])
@@ -52,17 +28,19 @@ const Resume = () => {
   const [additionalComments, setAdditionalComments] = useState('')
   const [jobDescription, setJobDescription] = useState('')
 
+  // State hooks for UI feedback and loading
   const [isWarningOpen, setIsWarningOpen] = useState(false)
   const [aiResponse, setAiResponse] = useState('')
   const [isLoading, setIsLoading] = useState(false)
+  const [copySuccess, setCopySuccess] = useState('') // New state for copy success message
 
   const responseRef = useRef(null)
-  const muiTheme = useTheme()
 
   // Chat limit state
   const [chatCount, setChatCount] = useState(0)
-  const chatLimit = 5
+  const chatLimit = 20
 
+  // Function to reset chat count if a new day has started
   const resetChatCountIfNeeded = () => {
     const savedDate = localStorage.getItem('resumeDate')
     const today = new Date().toDateString()
@@ -75,6 +53,7 @@ const Resume = () => {
     }
   }
 
+  // useEffect to initialize chat count and load last resume
   useEffect(() => {
     resetChatCountIfNeeded()
 
@@ -85,6 +64,7 @@ const Resume = () => {
     }
   }, [])
 
+  // Function to increment chat count
   const incrementChatCount = () => {
     setChatCount((prevCount) => {
       const newCount = prevCount + 1
@@ -94,14 +74,17 @@ const Resume = () => {
     })
   }
 
+  // Handler for Generate Resume button click
   const handleGenerateClick = () => {
     setIsWarningOpen(true)
   }
 
+  // Handler to close the warning dialog
   const handleCloseWarning = () => {
     setIsWarningOpen(false)
   }
 
+  // Handler to confirm resume generation
   const handleConfirmWarning = async () => {
     if (chatCount >= chatLimit) {
       alert(
@@ -118,7 +101,6 @@ const Resume = () => {
     const sanitizedDescription = DOMPurify.sanitize(description)
     const sanitizedAdditionalComments = DOMPurify.sanitize(additionalComments)
     const sanitizedJobDescription = DOMPurify.sanitize(jobDescription)
-    // Similarly sanitize other fields as needed
 
     const message = {
       description: sanitizedDescription,
@@ -132,60 +114,61 @@ const Resume = () => {
     }
 
     const formattedMessage = `
-            I would like to create a professional resume based on the following information:
+      I would like to create a professional resume based on the following information:
 
-            *Job Description:*
-            ${message.jobDescription}
+      *Job Description:*
+      ${message.jobDescription}
 
-            *Personal Description:*
-            ${message.description}
+      *Personal Description:*
+      ${message.description}
 
-            **Work Experiences:**
-            ${message.workExperiences
-              .map(
-                (exp, index) => `
-            ${index + 1}. **Position:** ${exp.position}
-            **Company:** ${exp.company}
-            **Duration:** ${exp.startDate} - ${exp.endDate}
-            **Responsibilities:**
-            - ${exp.responsibilities.join('\n  - ')}
-            `
-              )
-              .join('\n')}
+      **Work Experiences:**
+      ${message.workExperiences
+        .map(
+          (exp, index) => `
+          ${index + 1}. **Position:** ${exp.position}
+          **Company:** ${exp.company}
+          **Duration:** ${exp.startDate} - ${exp.endDate}
+          **Responsibilities:**
+          - ${exp.responsibilities.join('\n  - ')}
+          `
+        )
+        .join('\n')}
 
-            **Education:**
-            ${message.educations
-              .map(
-                (edu, index) => `
-            ${index + 1}. **Degree:** ${edu.degree}
-            **Field of Study:** ${edu.fieldOfStudy}
-            **Institution:** ${edu.institution}
-            **Duration:** ${edu.startDate} - ${edu.endDate}
-            `
-              )
-              .join('\n')}
+      **Education:**
+      ${message.educations
+        .map(
+          (edu, index) => `
+          ${index + 1}. **Degree:** ${edu.degree}
+          **Field of Study:** ${edu.fieldOfStudy}
+          **Institution:** ${edu.institution}
+          **Duration:** ${edu.startDate} - ${edu.endDate}
+          `
+        )
+        .join('\n')}
 
-            **Skills:**
-            ${message.skills.join(', ')}
+      **Skills:**
+      ${message.skills.join(', ')}
 
-            **Projects:**
-            ${message.projects
-              .map(
-                (project, index) => `
-            ${index + 1}. **Project Name:** ${project.name}
-            **Role:** ${project.role}
-            **Description:** ${project.description}
-            `
-              )
-              .join('\n')}
+      **Projects:**
+      ${message.projects
+        .map(
+          (project, index) => `
+          ${index + 1}. **Project Name:** ${project.name}
+          **Role:** ${project.role}
+          **Description:** ${project.description}
+          `
+        )
+        .join('\n')}
 
-            **Additional Activities:**
-            ${message.additionalActivities.join(', ')}
+      **Additional Activities:**
+      ${message.additionalActivities.join(', ')}
 
-            **Additional Comments:**
-            ${message.additionalComments}
+      **Additional Comments:**
+      ${message.additionalComments}
 
-            Please organize this information into a well-formatted resume.`
+      Please organize this information into a well-formatted resume.
+    `
 
     console.log('Formatted Message:', formattedMessage)
 
@@ -207,10 +190,29 @@ const Resume = () => {
     }
   }
 
+  // Modified handleCopy Function
+  // This function removes Markdown syntax before copying the text
+  const handleCopy = () => {
+    if (aiResponse && aiResponse.response && aiResponse.response.response) {
+      const resumeMarkdown = aiResponse.response.response
+      const resumeText = removeMarkdown(resumeMarkdown) // Remove Markdown syntax
+      navigator.clipboard.writeText(resumeText).then(
+        function () {
+          setCopySuccess('Resume copied to clipboard!')
+          // Clear the message after a few seconds
+          setTimeout(() => setCopySuccess(''), 3000)
+        },
+        function (err) {
+          console.error('Could not copy text: ', err)
+        }
+      )
+    }
+  }
+
   return (
     <>
       {/* Header Section */}
-      <Container maxWidth="md" sx={{ mt: 4 }}>
+      <Container maxWidth="md" sx={{ mt: 4, mb: 4 }}>
         <Typography
           variant="h2"
           component="h1"
@@ -242,93 +244,39 @@ const Resume = () => {
 
       {/* Main Content */}
       <Container maxWidth="md">
-        {isLoading && <LinearProgress sx={{ mb: 2 }} />}
+        <ResumeForm
+          description={description}
+          setDescription={setDescription}
+          workExperiences={workExperiences}
+          setWorkExperiences={setWorkExperiences}
+          educations={educations}
+          setEducations={setEducations}
+          skills={skills}
+          setSkills={setSkills}
+          projects={projects}
+          setProjects={setProjects}
+          additionalActivities={additionalActivities}
+          setAdditionalActivities={setAdditionalActivities}
+          additionalComments={additionalComments}
+          setAdditionalComments={setAdditionalComments}
+          jobDescription={jobDescription}
+          setJobDescription={setJobDescription}
+          isLoading={isLoading}
+          chatCount={chatCount}
+          chatLimit={chatLimit}
+          handleGenerateClick={handleGenerateClick}
+        />
 
-        <Paper
-          elevation={3}
-          sx={{
-            p: { xs: 2, sm: 4, md: 6 },
-            mb: { xs: 4, sm: 6, md: 8 },
-            position: 'relative',
-          }}
-        >
-          <Suspense fallback={<CenteredCircleLoader />}>
-            <Description
-              description={description}
-              setDescription={setDescription}
-            />
-            <WorkExperience
-              workExperiences={workExperiences}
-              setWorkExperiences={setWorkExperiences}
-            />
-            <Education educations={educations} setEducations={setEducations} />
-            <Skills skills={skills} setSkills={setSkills} />
-            <Projects projects={projects} setProjects={setProjects} />
-            <AdditionalActivities
-              additionalActivities={additionalActivities}
-              setAdditionalActivities={setAdditionalActivities}
-            />
-            <AdditionalComments
-              additionalComments={additionalComments}
-              setAdditionalComments={setAdditionalComments}
-            />
-            <JobDescription
-              jobDescription={jobDescription}
-              setJobDescription={setJobDescription}
-            />
-          </Suspense>
+        {isLoading && <CenteredCircleLoader />}
 
-          <Button
-            variant="contained"
-            color="primary"
-            onClick={handleGenerateClick}
-            fullWidth
-            sx={{
-              mt: 4,
-              py: 1.5,
-              px: 4,
-              fontWeight: 'bold',
-              fontSize: { xs: '0.8rem', sm: '1rem', md: '1.1rem' },
-            }}
-            disabled={isLoading || chatCount >= chatLimit}
-          >
-            {chatCount >= chatLimit
-              ? 'Daily Limit Reached'
-              : isLoading
-                ? 'Generating...'
-                : 'Generate Resume'}
-            <DoubleArrowRoundedIcon sx={{ ml: 0.5 }} />
-          </Button>
-
-          <Typography
-            variant="caption"
-            color="textSecondary"
-            align="center"
-            sx={{ mt: 1 }}
-          >
-            You have used {chatCount} out of {chatLimit} daily resume
-            generations.
-          </Typography>
-
-          {isLoading && <CenteredCircleLoader />}
-
-          {aiResponse && (
-            <Paper
-              elevation={2}
-              sx={{
-                mt: 6,
-                p: { xs: 2, sm: 3, md: 4 },
-                backgroundColor: muiTheme.palette.background.default,
-              }}
-              ref={responseRef}
-            >
-              <Typography variant="h5" gutterBottom>
-                Your Resume Template
-              </Typography>
-              <SafeMarkdown content={aiResponse.response.response} />
-            </Paper>
-          )}
-        </Paper>
+        {aiResponse && (
+          <ResumeResult
+            aiResponse={aiResponse}
+            copySuccess={copySuccess}
+            handleCopy={handleCopy}
+            responseRef={responseRef}
+          />
+        )}
       </Container>
 
       <Suspense fallback={<div>Loading...</div>}>
